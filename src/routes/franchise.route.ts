9f55e64d6e523d5223462e6481815f43e29c5658
Cleiton <cleiton.biou@gmail.com>
2022-11-20 00:44:02 -0300

    limite de requisições
    Signed-off-by: Cleiton <cleiton.biou@gmail.com>

--- src/routes/franchise.route.ts
+++ src/routes/franchise.route.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
+import rateLimit, { Options, MemoryStore } from "express-rate-limit";
+
 import { franchiseSchema, loginSchema } from "../helpers/yup-schemas";
 import FranchiseController from "../controllers/franchise.controller";
 import validateMiddleware from "../middleware/validate";
 import authMiddleware from "../middleware/authentication";
 
 const franchiseRoutes = Router();
+
+const rateLimitConfig: Partial<Options> = {
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5,
+  store: new MemoryStore(),
+  handler: (req, res) => {
+    res.status(429).json({ error: true, message: "Too many requests" });
+  },
+};
 
 franchiseRoutes.post(
   "/franchise",
@@ -13,6 +24,7 @@ franchiseRoutes.post(
 );
 franchiseRoutes.post(
   "/franchise/login",
+  rateLimit(rateLimitConfig),
   validateMiddleware(loginSchema),
   FranchiseController.login
 );
@@ -24,10 +36,12 @@ franchiseRoutes.put(
 );
 franchiseRoutes.post(
   "/franchise/forgot-password",
+  rateLimit(rateLimitConfig),
   FranchiseController.forgotPassword
 );
 franchiseRoutes.post(
   "/franchise/reset-password",
+  rateLimit(rateLimitConfig),
   FranchiseController.resetPassword
 );
 
