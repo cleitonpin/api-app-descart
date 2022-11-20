419d78c6b0b7dfab0d006ce835359ad655948d3c
Cleiton <cleiton.biou@gmail.com>
2022-11-20 20:47:15 -0300

    cors list allowed urls
    Signed-off-by: Cleiton <cleiton.biou@gmail.com>

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,6 +1,8 @@
 export const allowlist = [
   "http://127.0.0.1:5173",
   "https://admin-descart-farm-app.vercel.app",
+  "https://admin-descart-farm-app-git-master-cleitonpin.vercel.app",
+  "https://admin-descart-farm-app.vercel.app",
 ];
 
 export const corsOptionsDelegate = (req: any, callback: any) => {
